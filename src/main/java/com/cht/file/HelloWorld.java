6a58faf1da0d0a85fd9e88520226ab0ae1ef21d4
chenghongtao <18729553226@163.com>
2018-05-09 23:55:31 +0800

    第二次提交

---
+++ src/main/java/com/cht/file/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.cht.file;
+
+import java.util.Date;
+
+public class HelloWorld {
+   public static void main(String[] args) {
+	System.out.println(new Date());
+}
+}
---
+++ src/test/java/com/cht/struts/test/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.cht.struts.test;
+
+import java.util.Date;
+
+public class HelloWorld {
+   public static void main(String[] args) {
+	System.out.println(new Date());
+}
+}
