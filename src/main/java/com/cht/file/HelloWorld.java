41da1e480c362518ee934bbd72c311729b5477bc
chenghongtao <18729553226@163.com>
2018-05-10 00:00:27 +0800

    第三次提交

--- src/main/java/com/cht/file/HelloWorld.java
+++ src/main/java/com/cht/file/HelloWorld.java
@@ -1,9 +1,10 @@
 package com.cht.file;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class HelloWorld {
    public static void main(String[] args) {
-	System.out.println(new Date());
+	System.out.println(new SimpleDateFormat("yyyy-mm-dd hh:mm:ss").format(new Date()));
 }
 }
